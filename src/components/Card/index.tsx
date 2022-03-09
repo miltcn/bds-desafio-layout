a28c6fd65c17f7bed5cfd191d874442b307eafaa
Milton <milton.hneto@gmail.com>
2022-03-09 15:37:54 -0300

    Criado componente Card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,20 @@
+import CarCard from "../../assets/images/car-card.png";
+import "./styles.css";
+
+function Card() {
+  return (
+    <div className="card-container">
+      <div id="car-card">
+        <img src={CarCard} alt="Carro vermelho" />
+      </div>
+      <h1>Audi Supra TT</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
+        nisi
+      </p>
+      <a href="/">COMPRAR</a>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/Catalago/index.tsx
+++ src/pages/Catalago/index.tsx
@@ -1,8 +1,14 @@
 import Buscar from "components/Buscar";
+import Card from "components/Card";
 import "./styles.css";
 
 const Catalogo = () => {
-  return <Buscar />;
+  return (
+    <>
+      <Buscar />
+      <Card />
+    </>
+  );
 };
 
 export default Catalogo;
